cbe076e0dec4b6bd42f0f46d164fc5ef6e805f28
IDontNeedEyes <100826756+IDontNeedEyes@users.noreply.github.com>
2022-03-09 08:33:01 +0300

    Add files via upload

---
+++ GryazevClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class ProjectAssist
+    {
+        /// <summary>
+        /// Создает строку чисел от 1 до N
+        /// </summary>
+        /// <param name="lastNumber">Число N</param>
+        /// <returns>Возвращает строку чисел</returns>
+        public static string GetStringOfNumbers(int lastNumber)
+        {
+            if (lastNumber > 0)
+            {
+                string result = "";
+
+                for (int i = 1; i <= lastNumber; i++)
+                {
+                    result += $"{i} ";
+                }
+
+                return result;
+            }
+            else throw new IndexOutOfRangeException("Введенное значение меньше или равно 0");
+        }
+
+        /// <summary>
+        /// Создает массив строк, которые вместе образуют квадрат, со стороной N
+        /// </summary>
+        /// <param name="squareSideSize">Размер стороны квадрата</param>
+        /// <returns>Массив строк</returns>
+        public static string[] GetStringSquare(int squareSideSize)
+        {
+            if (squareSideSize > 1)
+            {
+                /// Создание переменных
+                string[] result = new string[squareSideSize];
+                string string1 = "";
+                string string2 = "";
+
+                /// Заполнение шаблонов строк
+                for (int i = 0; i < squareSideSize; i++)
+                {
+                    if (i == 0 || i == squareSideSize - 1)
+                    {
+                        string2 += "* ";
+                    }
+                    else 
+                        string2 += "  ";
+
+                    string1 += "* ";
+                }
+
+                /// Построение квадрата из шаблонов 
+                for (int i = 0; i < squareSideSize; i++)
+                {
+                    if(i == 0 || i == squareSideSize - 1)
+                    {
+                       result[i] = string1;
+                    }
+                    else
+                        result[i] = string2;
+                }
+
+                return result;
+            }
+            else throw new IndexOutOfRangeException("Введенное значение меньше или равно 1");
+        }
+    }
+
+
+}
---
+++ GryazevProgram.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class GryazevProgram
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i == 1; i++)
+            {
+                string choice = "";
+               
+                /// Выбор действия
+                for (int f = 1; f == 1; f++)
+                {
+                    /// Вывод указаний пользователю на экран
+                    Console.WriteLine("Создать квадрат(1) или строку чисел(2)?");
+
+                    choice = Console.ReadLine();
+
+                    /// Обработка выбора пользователя
+                    switch (choice.Trim())
+                    {
+                        case ("1"):
+                            Console.WriteLine("Введите число больше 1");
+                            break;
+
+                        case ("2"):
+                            Console.WriteLine("Введите число больше 0");
+                            break;
+
+                        default:
+                            Console.WriteLine("Ошибка: Ответ введен неправильно. Нужно ввести 1 или 2");
+                            f = 0;
+                            break;
+                    }
+                }
+
+                /// Проверка типа вводимых данных
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    /// Проверка значений и вывод строки чисел
+                    if (result > 0 && choice.Trim() == "2")
+                    {
+                        Console.WriteLine(ProjectAssist.GetStringOfNumbers(result));
+                    }
+                    else if (choice.Trim() == "1")
+                        Console.WriteLine("");
+                    else
+                        Console.WriteLine("Ошибка: Введенное число равно или меньше 0");
+
+                    /// Проверка значений и вывод квадрата из строк
+                    if (result > 1 && choice.Trim() == "1")
+                    {
+                        string[] square = ProjectAssist.GetStringSquare(result);
+
+                        for (int s = 0; s < result; s++)
+                        {
+                            Console.WriteLine(square[s]);
+                        }
+                    }
+                    else if (choice.Trim() == "2")
+                        Console.WriteLine("");
+                    else 
+                        Console.WriteLine("Ошибка: Введенное число равно или меньше 1");
+                }
+                else
+                    Console.WriteLine("Ошибка: Введено не число");
+
+                /// Выбор выхода или продолжения работы программы
+                for (int j = 1; j == 1; j++)
+                {
+                    Console.WriteLine("Закончить работу?(Y/N)");
+                    string answer = Console.ReadLine();
+                    
+                    /// Обработка ответа
+                    switch (answer)
+                    {
+                        case ("Y"):
+                            i = 2;
+                            j = 2;
+                            break;
+
+                        case ("N"):
+                            i = 0;
+                            j = 2;
+                            break;
+
+                        default:
+                            Console.WriteLine("Ошибка: Ответ введен неправильно. Нужно ввести Y или N");
+                            j = 0;
+                            i = 0;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
